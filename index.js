638b89ad997e14e8e80f2614b6a3a073ed11c85b
Roger Lo <sobadrush@icloud.com>
2023-04-07 17:30:13 +0800

    add index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+
+// import { genRandomInt } from './util.js';
+
+import * as myUtil from './util.js';
+
+// 測試 JS export/import
+// console.log('myVal >>> ', myUtil.myVal);
+// console.log('myObj2 >>> ', myUtil.myObj2);
+// console.log('myObj3 >>> ', myUtil.myObj3);
+
+// const myPromise = new Promise((resolve, reject) => {
+//     let rand = myUtil.genRandomInt(1, 10);
+//     console.log(`rand = ${rand}`);
+//     setTimeout(() => {
+//         if (rand % 2 == 0) {
+//             resolve("Promise 實現 - resolve");
+//         } else {
+//             reject("Promise 拒絕 - reject");
+//         }
+//     }, 3000);
+// });
+
+// myPromise
+//     .then(val => console.log(`then → ${val}`))
+//     .catch(reason => console.error(`catch → ${reason}`));
+
